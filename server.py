df8b98afe41bb6661c2982b32dc528e66fd90e8f
Arya Kumar <thearyaskumar@icloud.com>
2019-09-27 14:50:45 -0400

    added boilerplate

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from os.path import join, dirname, realpath
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, g
 
 app = Flask(__name__, static_url_path="")
 static_file_dir = join(dirname(realpath(__file__)), "static")
@@ -17,8 +17,34 @@ def send_statics(path):
 
 @app.route("/api/temp")
 def temp_api():
-    return "I DID A THING."
+    return "VAL is at {}".format(g["val"])
+
+
+@app.route("/api/beep/<speaker_id>")
+def beep(speaker_id):
+    """ Makes the speaker with a certain ID, beep to
+    indicate that the music is changing. """
+    # TODO: this.
+
+
+@app.route("/api/update_room/<phone_id>/<speaker_id>")
+def update_room(phone_id, speaker_id):
+    """ Adds the phones to the global context. Accordingly
+        adjusts the music playing in each room, to match
+        what all the users in the room would want to hear
+        using the favorites API. """
+    # TODO: this
+
+
+def gather_rooms():
+    """ Using the Sonos API, turns each speaker into
+        its own group, and saves those rooms to the 
+        global context. Each speaker has a UID, which
+        is also linked to an NFC tag. The exact 
+        implementation of this is unclear. """
+    # TODO: this
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    g = {"rooms": gather_rooms()}
+    app.run(host="0.0.0.0", port=3000, debug=True)
