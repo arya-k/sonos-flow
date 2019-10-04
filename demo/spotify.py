83a40ea06331cb4fdf1f317b947a54c6ea390eda
Arya Kumar <thearyaskumar@icloud.com>
2019-10-04 16:21:18 -0400

    added avg playlist vector

--- demo/spotify.py
+++ demo/spotify.py
@@ -91,9 +91,14 @@ def load_tree():
     with open("data.pkl", "rb") as f:
         data_metadata, data_vectors = pickle.load(f)
 
+    track_id_to_index = {t[1]: i for i, t in enumerate(data_metadata)}
     tree = spatial.KDTree(data_vectors)
 
-    return data_metadata, data_vectors, tree
+    return {
+        "data_vectors": data_vectors,
+        "track_id_to_index": track_id_to_index,
+        "tree": tree,
+    }
 
 
 #####################
@@ -124,12 +129,52 @@ def get_sonos_playlist_names(sp):
     return [p["name"].split("SONOS_")[1] for p in all_playlists]
 
 
+def get_playlist_vector(sp, track_ids, track_id_to_index, data_vectors):
+    """ Return the average vector for a whole playlist. """
+
+    to_ret = [0] * len(csv_vector_headers)
+    for track_id in track_ids:
+        for i, val in enumerate(data_vectors[track_id_to_index[track_id]]):
+            to_ret[i] += val
+
+    return [x / len(track_ids) for x in to_ret]
+
+
 ########
 # MAIN #
 ########
 
 if __name__ == "__main__":
     sp = get_auth()
-    from pprint import pprint
 
-    pprint(get_sonos_playlists(sp))
+    matching_data = load_tree()
+
+    playlists = sp.user_playlists(USER_ID)["items"]
+
+    for playlist in playlists:
+        all_track_ids = []
+
+        results = sp.user_playlist(USER_ID, playlist["id"], fields="tracks,next")
+        tracks = results["tracks"]
+
+        all_track_ids += [t["track"]["id"] for t in tracks["items"]]
+
+        while tracks["next"]:
+            tracks = sp.next(tracks)
+            all_track_ids += [t["track"]["id"] for t in tracks["items"]]
+
+        print(
+            playlist["name"],
+            len(all_track_ids),
+            "[{}]".format(
+                ", ".join(
+                    "{:.3f}".format(x)
+                    for x in get_playlist_vector(
+                        sp,
+                        all_track_ids,
+                        matching_data["track_id_to_index"],
+                        matching_data["data_vectors"],
+                    )
+                )
+            ),
+        )
